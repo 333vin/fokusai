5713a6e0c1ca4e9b8a3bc64b33a07cfc4c54d637
vin <vinvibro31@gmail.com>
2026-07-13 00:52:40 -0400

    Initial Commit

---
+++ fokusai/fokusaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  fokusaiApp.swift
+//  fokusai
+//
+//  Created by Navneet Sharma on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct fokusaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
